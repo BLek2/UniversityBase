a9105c6ad0aa235ea3d6509fe126c403267ad5fb
Andrew Romanuk <blek213@gmail.com>
2017-07-23 19:36:34 +0300

    Ability to add new student and group

---
+++ UniversityBase/Models/StudentUniversityContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace UniversityBase.Models
+{
+    public class StudentUniversityContext:DbContext
+    {
+        public StudentUniversityContext() : base("StudentContext") { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Mark> Marks { get; set; }
+    }
+}
