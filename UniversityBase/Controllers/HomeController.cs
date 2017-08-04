247ee1a9ce3606d244d1bdd2b5a48d050b01b744
Andrew Romanuk <blek213@gmail.com>
2017-08-04 21:28:51 +0300

    Added a search engine for Student

--- UniversityBase/Controllers/HomeController.cs
+++ UniversityBase/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Collections;
 using UniversityBase.Models;
+
 
 namespace UniversityBase.Controllers
 {
@@ -36,20 +38,37 @@ namespace UniversityBase.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult StudentResult(string FindName, string FindSurname)
+        public ActionResult StudentResult(string FindName, string FindSurname, int? Age, int? Course)
         {
+            if(Age == null){            
+                    Age = 0;
+            }
+            if(Course == null){                
+                    Course = 0;
+            }
+
             System.Data.SqlClient.SqlParameter parametr1 = new System.Data.SqlClient.SqlParameter("@Name", FindName);
             System.Data.SqlClient.SqlParameter parametr2 = new System.Data.SqlClient.SqlParameter("@Surname", FindSurname);
-            var StudentsByName = UniversityDb.Students.SqlQuery("SELECT * FROM dbo.Students WHERE Name LIKE @Name OR Surname LIKE @Surname  ", parametr1, parametr2);
+            System.Data.SqlClient.SqlParameter parametr3 = new System.Data.SqlClient.SqlParameter("@Age", Age);
+            System.Data.SqlClient.SqlParameter parametr4 = new System.Data.SqlClient.SqlParameter("@Course", Course);
+ 
+            var StudentsByName = UniversityDb.Students.SqlQuery("SELECT * FROM dbo.Students " +
+                "WHERE Name LIKE @Name OR Surname LIKE @Surname OR Age=@Age OR Course=@Course  ", parametr1, parametr2,parametr3,parametr4);
+
+
+
+
             ViewBag.StudentsByName = StudentsByName;
+            
+
+            
 
             return PartialView();
         }
         
         public ActionResult AdvanceSearch()
         {
-            SelectList selectedGroup = new SelectList(UniversityDb.Groups, "Id", "NameOfGroup");
-            ViewBag.selectedGroup = selectedGroup;
+           
 
             return PartialView();
         }
